ebd3d8c2ddf9588339d1c012a1a81e41baf4a9f1
Furkan Arslan <2200656003nku.edu.tr>
2025-11-05 23:32:04 +0300

    Add project files.

---
+++ hali_takip/hali_takip/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace hali_takip.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
